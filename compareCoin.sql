adf62c946d5b23e9e6779e7d0f45ef68c65a50d3
florentious <florentious09@gmail.com>
2020-01-20 18:55:42 +0900

    2020.01.20-v.1

---
+++ compareCoin.sql
@@ -0,0 +1,49 @@
+CREATE TABLE dowjones (
+	dj_date DATE PRIMARY KEY,
+	dj_close DOUBLE,
+	dj_open DOUBLE,
+	dj_high DOUBLE,
+	dj_low DOUBLE,
+	dj_volume DOUBLE
+
+);
+
+SELECT * FROM dowjones;
+
+CREATE TABLE gold (
+	gold_date DATE PRIMARY KEY,
+	gold_close DOUBLE,
+	gold_open DOUBLE,
+	gold_high DOUBLE,
+	gold_low DOUBLE,
+	gold_volume DOUBLE
+ 
+);
+
+SELECT * FROM gold;
+
+CREATE TABLE bitcoin (
+	bit_date DATE PRIMARY KEY,
+	bit_close DOUBLE,
+	bit_open DOUBLE,
+	bit_high DOUBLE,
+	bit_low DOUBLE,
+	bit_volume DOUBLE
+ 
+);
+
+SELECT * FROM bitcoin;
+
+CREATE TABLE ether (
+	eth_date DATE PRIMARY KEY,
+	eth_close DOUBLE,
+	eth_open DOUBLE,
+	eth_high DOUBLE,
+	eth_low DOUBLE,
+	eth_volume DOUBLE
+ 
+);
+
+SELECT * FROM ether;
+
+DELETE FROM ether;
