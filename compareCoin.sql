893640e5df4dfc392d0a027a5412683fefb5ffd8
florentious <florentious09@gmail.com>
2020-02-06 18:45:52 +0900

    2020.02.06

--- compareCoin.sql
+++ compareCoin.sql
@@ -8,8 +8,6 @@ CREATE TABLE dowjones (
 
 );
 
-SELECT * FROM dowjones;
-
 CREATE TABLE gold (
 	gold_date DATE PRIMARY KEY,
 	gold_close DOUBLE,
@@ -19,8 +17,6 @@ CREATE TABLE gold (
 	gold_volume DOUBLE
  
 );
-
-SELECT * FROM gold ORDER BY gold_date desc;
 
 CREATE TABLE bitcoin (
 	bit_date DATE PRIMARY KEY,
@@ -32,7 +28,6 @@ CREATE TABLE bitcoin (
  
 );
 
-SELECT * FROM bitcoin;
 
 CREATE TABLE ether (
 	eth_date DATE PRIMARY KEY,
@@ -44,10 +39,76 @@ CREATE TABLE ether (
  
 );
 
-SELECT * FROM bitcoin
+CREATE TABLE al_close (
+	cl_date DATE PRIMARY KEY,
+	cl_bitcoin DOUBLE,
+	cl_gold DOUBLE,
+	cl_dowjones double
+	
 
-SELECT * FROM ether ORDER BY eth_date desc;
+);
 
-DELETE FROM ether;
+CREATE TABLE makedData (
+	md_date DATE PRIMARY KEY,
+	
+	md_bit_close DOUBLE,
+	md_bit_ma5 DOUBLE,
+	md_bit_ma60 DOUBLE,
+	md_bit_dif DOUBLE,
+	md_bit_dif_b1 DOUBLE,
+	
+	md_gol_close DOUBLE,
+	md_gol_ma5 DOUBLE,
+	md_gol_ma60 DOUBLE,
+	md_gol_dif DOUBLE,
+	md_gol_dif_b1 DOUBLE,
+	
+	
+	md_dj_close DOUBLE,
+	md_dj_ma5 DOUBLE,
+	md_dj_ma60 DOUBLE,
+	md_dj_dif DOUBLE,
+	md_dj_dif_b1 DOUBLE,
+	
+	md_bit_change INT,
+	md_gol_change INT,
+	md_dj_change int
+	
+);
 
-SELECT * FROM dowjones ORDER BY dj_date DESC;
+CREATE TABLE pred_clf (
+	pc_date DATE PRIMARY KEY,
+	
+	pc_bit_arima DOUBLE,
+	pc_gol_arima DOUBLE,
+	pc_dj_arima DOUBLE,
+	
+	pc_bit_vote INT,
+	pc_bit_knc INT,
+	pc_bit_svc INT,
+	pc_bit_rfc INT,
+	pc_bit_xgbc INT,
+	pc_bit_lgbc INT,
+	
+	pc_gol_vote INT,
+	pc_gol_knc INT,
+	pc_gol_svc INT,
+	pc_gol_rfc INT,
+	pc_gol_xgbc INT,
+	pc_gol_lgbc INT,
+	
+	pc_dj_vote INT,
+	pc_dj_knc INT,
+	pc_dj_svc INT,
+	pc_dj_rfc INT,
+	pc_dj_xgbc INT,
+	pc_dj_lgbc INT
+	
+
+);
+
+SELECT * FROM makedData;
+
+SELECT * FROM pred_clf;
+
+SELECT * FROM pred_clf ORDER BY pc_date DESC LIMIT 0,1;
